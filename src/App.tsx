90571d2fbb7580bab6f6b415e77ee74b23b092f2
Erica <ericaspitz522@gmail.com>
2021-09-13 11:46:47 -0600

    Add basic html layout for card sections

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,33 @@ function App() {
         //   --> more detailed weather info for today's date
         //Denver, LA, NY, Phoenix daily weather forecast (displayed on homepage ) CityForecastCard
 
+        // City Forecast Card layout
+        // <section>
+        //   <h2>City Name<h2>
+        //   <img>weather logo</img>
+        //   <p>HiTemp</p><p>LowTemp</p> //inline
+        //   <p>Chance of Precip</p>
+        // </section>
+
       <DetailedForecastContainer /> <DetailedForecastCard />
       // within CityForecastContainer will have DetailedForecastContainer (movieDetails) --> 5 day forecast details with back button
       // DetailedForecastContainer holds DailyCards for each day?
+
+      // Detailed Forecast Card layout
+      // <section>
+      //   <h2>City Name<h2>
+      //   <img>weather logo</img>
+      //   <p>Description</p>
+      //   <ul>
+      //     <li>HiTemli</li><li>LowTemli</li> //inline
+      //     <li>Chance of Precili</li>
+      //     <li>Humidity</li>
+      //     <li>Feels like Temli</li>
+      //     <li>Wind Slieed</li>
+      //     <li>Visibility</li>
+      //     <li>Sunrise/Sunset
+      //   </ul>
+      // </section>
 
     </div>
   );
