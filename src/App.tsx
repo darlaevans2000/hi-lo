7e065d42e3e248af154f162ea504a14f4b2d0989
Erica <ericaspitz522@gmail.com>
2021-09-13 11:28:03 -0600

    Pseudo out components

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header /> // h1 app - HiLo
+      <Form /> // search bar for finding cities by name or zip, displayed just on homepage
+      <CityForecastContainer /> <CityForecastCard />
+        //   --> more detailed weather info for today's date
+        //Denver, LA, NY, Phoenix daily weather forecast (displayed on homepage ) CityForecastCard
+
+      <DetailedForecastContainer /> <DetailedForecastCard />
+      // within CityForecastContainer will have DetailedForecastContainer (movieDetails) --> 5 day forecast details with back button
+      // DetailedForecastContainer holds DailyCards for each day?
+
     </div>
   );
 }
