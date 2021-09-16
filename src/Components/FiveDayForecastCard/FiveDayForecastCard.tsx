515a181ce1134b64b440e85593cd4b7b4b572de0
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-09-16 14:16:17 -0700

    Update interface for 5 day card

--- src/Components/FiveDayForecastCard/FiveDayForecastCard.tsx
+++ src/Components/FiveDayForecastCard/FiveDayForecastCard.tsx
@@ -4,9 +4,21 @@ import { Forecast } from "../../model/Weather";
 interface FiveDayForecastCardProps {
   key: number;
   temp: number;
+  dayTwo: {
+    humidity: number;
+    temp: {
+      max: number;
+      min: number;
+    }
+    uvi: number;
+    weather: [{
+      description: string;
+      icon: string;
+    }]
+  }
 }
 
-export const FiveDayForecastCard: FC<FiveDayForecastCardProps> = ({ key, temp}) => {
+export const FiveDayForecastCard: FC<FiveDayForecastCardProps> = ({ key, temp, dayTwo, dayThree, dayFour, dayFive}) => {
   return (
     <>
       <p>CARD TEMP:{temp}</p>
