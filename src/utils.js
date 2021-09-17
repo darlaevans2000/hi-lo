0e309bec375f92f14ffdca22ea4524c2648c3707
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-09-17 10:06:03 -0700

    Clean function to convert degrees to F

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 export const cleanCurrentWeatherData = (data) => {
     data.main.temp_max = Math.round(((((data.main.temp_max) - 273.15) * 1.8)) + 32);
     data.main.temp_min = Math.round(((((data.main.temp_min) - 273.15) * 1.8)) + 32);
+
   return data
 }
 
@@ -10,13 +11,22 @@ export const cleanFiveDayForecastData = (data) => {
      const newDate = (new Date(day.dt * 1000)).toString().split(" ").slice(0, 1).join(" ").toUpperCase();
      const newHiTemp = Math.round(((((day.temp.max) - 273.15) * 1.8)) + 32);
      const newLowTemp = Math.round(((((day.temp.min) - 273.15) * 1.8)) + 32);
+
      day.dt = newDate;
      day.temp.max = newHiTemp;
      day.temp.min = newLowTemp;
    }
    })
-  return data
+   data.hourly.forEach((hour, i) => {
+      const newTemp = Math.round(((((hour.temp) - 273.15) * 1.8)) + 32);
+      hour.temp = newTemp;
+
+})
+  data.current.temp = Math.round(((((data.current.temp) - 273.15) * 1.8)) + 32)
+return data
 }
+
+
 
 /*export const cleanCurrentWeatherData = (data) => {
   if (Array.isArray(data)) {
