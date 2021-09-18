62bb9284dd7a8388ff3a212f2c721c794c9b6c7e
darlaevans2000 <darlaevans2000@gmail.com>
2021-09-18 16:46:11 -0600

    Add functioning search suggestion bar

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,12 +1,24 @@
 import React, { FC, useState } from 'react'
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from 'react-places-autocomplete'
 import './Form.css'
 interface SearchCityProps {
   onSearch: (cityInput: string) => void;
 }
 
 export const Form: FC<SearchCityProps> = ({onSearch}) => {
-  const [cityInput, setCityInput] = useState('')
+  const [cityInput, setCityInput] = useState('');
   const disableSearch = cityInput.trim() === '';
+  const [coordinates, setCoordinates] = useState({});
+ 
+  const handleSelect = async (value:string) => {
+    const results = await geocodeByAddress(value)
+    const latLng = await getLatLng(results[0])
+    setCityInput(value)
+    setCoordinates(latLng)
+  };
 
   const addCity = () => {
     onSearch(cityInput);
@@ -15,15 +27,29 @@ export const Form: FC<SearchCityProps> = ({onSearch}) => {
 
   return (
     <div className='searchForm'>
-      <form>
-        <label>
-        Search by City 
-        <input className="search-by-city" type="text" value={cityInput}
-               onChange={e => setCityInput(e.target.value)}/>
-        </label>
-        <button className="search-btn" type='submit' onClick={addCity} disabled={disableSearch}>Search</button>
-      </form>
-    </div>
+      <PlacesAutocomplete
+        value={cityInput}
+        onChange={setCityInput}
+        onSelect={handleSelect}
+        >{({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
+        <div>
+          <label>Search by City  </label>
+          <input {...getInputProps({placeholder: "Type City"})}/>
+          <div>
+            {loading ? <div>loading...</div> : null}
+            {suggestions.map((suggestion) => {
+              const style = suggestion.active
+               ? {backgroundColor: "#668b8a"}
+               :  {backgroundColor: "#ffff"};
+               
+              return <div{...getSuggestionItemProps(suggestion, {style})}>{suggestion.description}</div>
+            })}
+             <button className="search-btn" type='submit' onClick={addCity} disabled={disableSearch}>Search</button>
+          </div>
+        </div>
+        )}
+      </PlacesAutocomplete>
+      </div>
   )
 }
 
