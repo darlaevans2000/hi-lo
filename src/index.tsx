0b351c1a29afae8f7f3e8ce7cfe252acb192c0a6
darlaevans2000 <darlaevans2000@gmail.com>
2021-09-20 20:33:50 -0600

    Updates

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App/App';
-import { BrowserRouter } from 'react-router-dom';
-
-const router = <BrowserRouter> <App /> </BrowserRouter>;
-
-ReactDOM.render(router, document.getElementById('root'));
